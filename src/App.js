5b694ab36fdb0a3d30d9f2896e8be6211b48d5d9
tomoya-onishi <tomoya-onishi@x-power.co.jp>
2020-11-04 10:44:48 +0900

    パーツ配置

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import { isCompositeComponent } from 'react-dom/test-utils';
 class App extends Component {
   render(){
     return <div>
-        <p>404 NOT FOUND</p>
+        <header>404 NOT FOUND</header>
         <img src={NotFound}/>
+        <h1>I have bad news for you</h1>
+        <h2>The page you are looking for might be removed or is temporarily unavailable</h2>
+        <button>BACK TO HOMEPAGE</button>
     </div>
   }
 }
