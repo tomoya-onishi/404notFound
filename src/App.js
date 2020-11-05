e43b03500124da6c596dbc2e7e5e2504a119869d
tomoya-onishi <tomoya-onishi@x-power.co.jp>
2020-11-05 10:57:05 +0900

    スマホ対応

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import NotFound from './images/Scarecrow.png';
 import './App.css';
 import React,{Component} from 'react';
-import { isCompositeComponent } from 'react-dom/test-utils';
+import MediaQuery from "react-responsive";
+
 class App extends Component {
   render(){
     return <div>
-        <header>404 NOT FOUND</header>
-        <img src={NotFound}/>
-        <h1>I have bad news for you</h1>
-        <h2>The page you are looking for might be removed or is temporarily unavailable</h2>
-        <button>BACK TO HOMEPAGE</button>
+        <MediaQuery query='(min-width: 768px)'>
+          <header className="pcHeader">404 NOT FOUND</header>
+          <img className="pcImg" src={NotFound}/>
+          <h1 className="pcH1" >I have bad news for you</h1>
+          <h2 className="pcH2" >The page you are looking for might be removed or is temporarily unavailable</h2>
+          <button className="pcButton">BACK TO HOMEPAGE</button>
+          <footer className="pcFooter">Your name @ DevChallenges.io</footer>
+        </MediaQuery>
+        <MediaQuery query='(max-width: 768px)'>
+          <header className="smartHeader">404 NOT FOUND</header>
+          <img className="smartImg" src={NotFound}/>
+          <h1 className="smartH1">I have bad news for you</h1>
+          <h2 className="smartH2">The page you are looking for might be removed or is temporarily unavailable</h2>
+          <button className="smartButton">BACK TO HOMEPAGE</button>
+          <footer className="smartFooter">Your name @ DevChallenges.io</footer>
+        </MediaQuery>
     </div>
   }
 }
